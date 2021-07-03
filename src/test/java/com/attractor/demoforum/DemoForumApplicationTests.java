35825d2ecc5a5a01e4bccb884e0dc3ae44bc0ecc
NursultanCode <55210164+NursultanCode@users.noreply.github.com>
2021-07-03 12:15:34 +0600

    Starting project

---
+++ src/test/java/com/attractor/demoforum/DemoForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attractor.demoforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
